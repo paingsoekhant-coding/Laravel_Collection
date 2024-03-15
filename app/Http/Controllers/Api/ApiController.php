7f9aa01d755ed0c69d391409cd2e9f6fe93efe87
Paing Soe Khant <babyboss7916@gmail.com>
2024-03-15 12:03:27 +0630

    wrote register api

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2,15 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
     //Register Api (POST)
     public function register(Request $request)
     {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required','lowercase','email','unique:users'],
+            'password' => ['required','confirmed']
 
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+
+        ]);
+
+        return response()->json();
     }
 
 
@@ -23,7 +39,7 @@ class ApiController extends Controller
     //Profile Api (GET)
     public function profile()
     {
-        dd("hello middleware");
+
     }
 
 
