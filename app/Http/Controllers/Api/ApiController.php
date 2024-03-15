60c405a4dcf379b1f6fc99f48f6747b591b5d75f
Paing Soe Khant <babyboss7916@gmail.com>
2024-03-15 09:59:19 +0630

    setup api controller and write method

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //Register Api (POST)
+    public function register(Request $request)
+    {
+
+    }
+
+
+    //Login Api (POST)
+    public function login(Request $request)
+    {
+
+    }
+
+    //Profile Api (GET)
+    public function profile()
+    {
+
+    }
+
+
+    //Logout Api (GET)
+    public function logout()
+    {
+
+    }
+}
