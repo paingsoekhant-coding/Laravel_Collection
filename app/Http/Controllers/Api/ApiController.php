1bb208792d3d85ada29824ffea7210034391c063
Paing Soe Khant <babyboss7916@gmail.com>
2024-03-15 12:34:01 +0630

    wrote login api

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
@@ -12,13 +13,14 @@ class ApiController extends Controller
     //Register Api (POST)
     public function register(Request $request)
     {
+        //validation
         $request->validate([
             'name' => ['required'],
             'email' => ['required','lowercase','email','unique:users'],
             'password' => ['required','confirmed']
 
         ]);
-
+        //create user
         User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -26,13 +28,36 @@ class ApiController extends Controller
 
         ]);
 
-        return response()->json();
+        return response()->json(["status" => true , "message" => "User Created Successfully"]);
     }
 
 
     //Login Api (POST)
     public function login(Request $request)
     {
+        //validation
+        $request->validate([
+            'email' => ['required' , 'email' ],
+            'password' => ['required']
+        ]);
+
+        //check user login
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password])) {
+
+                $user = Auth::user();
+
+                $token = $user->createToken('myToken')->accessToken;
+
+                return response()->json(['status' => true , 'message' => 'Login Successfully' , 'token' => $token]);
+
+        }else{
+
+            return response()->json(['status' => false , 'message' => 'Invalid Login Details']);
+
+
+        }
 
     }
 
